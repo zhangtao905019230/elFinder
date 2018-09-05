bf6001a74d8299574bd6f5d1ab1082e5da49b3a5
zhangtao25 <zhangtao25@lenovo.com>
2018-09-05 18:08:14 +0800

    zt

--- php/connector.minimal.php
+++ php/connector.minimal.php
@@ -1,5 +1,14 @@
 <?php
 
+header('Access-Control-Allow-Methods:OPTIONS, GET, POST');
+header('Access-Control-Allow-Headers:x-requested-with');
+header('Access-Control-Max-Age:86400');
+header('Access-Control-Allow-Origin:http://localhost:8888');
+header('Access-Control-Allow-Credentials:true');
+header('Access-Control-Allow-Methods:GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
+header('Access-Control-Allow-Headers:Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With');
+
 error_reporting(0); // Set E_ALL for debuging
 
 // // To Enable(true) handling of PostScript files by ImageMagick
